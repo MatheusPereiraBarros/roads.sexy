a1ac7459cc6f5ee3179509f9e7d82475a183a254
Walter Zimmer <walterzimmer@gmx.de>
2019-11-24 09:53:34 +0100

    removed not used files

--- hackatum-2019/build/extern/gtest/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workstation/Desktop/roads.sexy/hackatum-2019/src/extern/gtest
-# Build directory: /home/workstation/Desktop/roads.sexy/hackatum-2019/build/extern/gtest
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
--- hackatum-2019/build/extern/gtest/googlemock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/workstation/Desktop/roads.sexy/hackatum-2019/src/extern/gtest/googlemock
-# Build directory: /home/workstation/Desktop/roads.sexy/hackatum-2019/build/extern/gtest/googlemock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
